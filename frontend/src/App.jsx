bdf0805fe25ac36f6b810df2b5778089a2d70f55
Iqra Asghar <iqraasghar022@gmail.com>
2025-04-27 15:29:22 +0500

    1st commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
